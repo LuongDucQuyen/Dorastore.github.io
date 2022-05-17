062b8fe6a07377187bdbc4182b52fdf986ac4f84
LuongDucQuyen <90146909+LuongDucQuyen@users.noreply.github.com>
2022-05-17 14:46:35 +0700

    Add files via upload

---
+++ js/Click.js
@@ -0,0 +1,24 @@
+var element = "";
+var showMenu = (id) => {
+  id.onclick = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (element == "") {
+      id.querySelector(".menu_list2").style.display = "block";
+      element = id;
+    } else if (element == id) {
+      element.querySelector(".menu_list2").style.display = "none";
+      element = "";
+    } else {
+      element.querySelector(".menu_list2").style.display = "none";
+      id.querySelector(".menu_list2").style.display = "block";
+      element = id;
+    }
+  };
+};
+var showMenu2 = (id) => {
+  id.querySelector(".menu_list3").style.display = "block";
+};
+var closeMenu2 = (id) => {
+  id.querySelector(".menu_list3").style.display = "none";
+};
